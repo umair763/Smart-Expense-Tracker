e61291f7f209276d32c776fc0326ccbbb90ee775
Muhammad Umair <93200938+umair763@users.noreply.github.com>
2025-02-28 11:07:59 +0500

    sidebar added

---
+++ client/src/app/layout/Header.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+function Header() {
+   return (
+      <>
+         <h1>Hello</h1>
+      </>
+   );
+}
+
+export default Header;
--- client/src/app/layout/Layout.jsx
+++ client/src/app/layout/Layout.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import SideBar from './Sidebar';
+import Header from './Header';
 
 function Layout({ children }) {
    return (
       <div className="flex flex-col min-h-screen bg-gray-50">
+         <Header />
          {/* Added some basic styling */}
-         <div className="flex-grow flex">
+         <div className="flex-grow flex h-screen">
             {/* Added Flex for Sidebar and Main content */}
             <SideBar />
             <main className="p-6 bg-gray-50 w-full">
